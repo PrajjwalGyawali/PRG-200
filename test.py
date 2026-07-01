fa1e1d32b42c3617f1eb486d67fd57245d16d232
PrajjwalGyawali <fluffkinsuspenze21@gmail.com>
2026-07-01 08:39:36 +0545

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-def remmitance(original_amount,agent_percentage_charge):
-    npr = 165 * original_amount
-    final_fee = npr - (agent_percentage_charge*npr)
-    return final_fee
-
-print(remmitance(100, 0.05)) 
